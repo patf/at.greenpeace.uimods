5d177d803f94eb5ad3b94162c39412c70ca40209
systopia <endres@systopia.de>
2017-02-17 11:23:56 +0100

    added settings spec

---
+++ settings/uimods.setting.php
@@ -0,0 +1,30 @@
+<?php
+/*-------------------------------------------------------+
+| Greenpeace UI Modifications                            |
+| Copyright (C) 2017 SYSTOPIA                            |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+/*
+* Settings metadata file
+*/
+return array(
+  'at.greenpeace.uimods' => array(
+    'group_name' => 'GP UIMods',
+    'group' => 'at.greenpeace.uimods',
+    'name' => 'at.greenpeace.uimods',
+    'type' => 'Array',
+    'default' => "",
+    'add' => '4.6',
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'description' => 'Stored information on custom fields',
+  ),
+ );
