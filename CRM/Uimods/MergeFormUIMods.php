ada18cd77de95f6e6a813ca6b5180d840cb606ff
Philipp Batroff <batroff@systopia.de>
2017-09-18 16:04:17 +0200

    fixed GP-1090

---
+++ CRM/Uimods/MergeFormUIMods.php
@@ -0,0 +1,34 @@
+<?php
+/*-------------------------------------------------------+
+| Greenpeace UI Modifications                            |
+| Copyright (C) 2017 SYSTOPIA                            |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+
+/**
+ * Minor Changes to the UI Merge form (GP-1090)
+ *  -
+ */
+class CRM_Uimods_MergeFromUIMods {
+
+  /**
+   * Page run hook for UI Merge
+   * @param $formName
+   * @param $form
+   */
+  public static function buildFormHook($formName, &$form) {
+
+    $script = file_get_contents(__DIR__ . '/../../js/merge_form_mods.js');
+    CRM_Core_Region::instance('page-footer')->add(array(
+      'script' => $script,
+    ));
+  }
+}
---
+++ js/merge_form_mods.js
@@ -0,0 +1,20 @@
+/*-------------------------------------------------------+
+| Greenpeace UI Modifications                            |
+| Copyright (C) 2017 SYSTOPIA                            |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+// GP-1090
+// click the membership new checkbox
+cj("#operation\\[move_rel_table_memberships\\]\\[add\\]").prop('checked', true);
+// disable the checkbox afterwards!
+cj("#operation\\[move_rel_table_memberships\\]\\[add\\]").attr('disabled', 'disabled');
+// FixMe: Not the cleanest way, but apparently checkboxes can't be set to readonly, and disabled doesn't transfer the value
+cj("#operation\\[move_rel_table_memberships\\]\\[add\\]").after('<input name="operation[move_rel_table_memberships][add]" type="hidden" value="1" />');
